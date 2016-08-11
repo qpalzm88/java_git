97da2d47215ff2e99cf3ee298483b5a2699f451e
qpalzm88 <qpalzm@list.ru>
2016-08-11 10:49:21 +0300

    create field

--- XO/src/com/company/Main.java
+++ XO/src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+        String playerName = new String();
+        System.out.println("Hello this is game XO");
+        playerName = howName();
+        Game game = new Game(playerName);
+        Field field=new Field();
+
+
+
+    }
+    private static String howName() throws IOException {
+        String str = new String();
+        System.out.println("What is your name? ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+           str = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
     }
 }
